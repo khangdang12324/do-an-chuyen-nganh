8c4cf9f362174d9f2887585a29fa1ddbae722a96
KhangDang <dangkhang.120304@gmail.com>
2025-08-27 23:21:54 +0700

    First test

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div>
+  <h1 className="text-4xl-font-bold text-rose-500">Welcome</h1>
+<Button>Click me</Button>
+</div>
+  );
+}
